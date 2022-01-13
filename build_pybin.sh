65488b6545c1263f8d9d0d40997df5bd87d9f461
Vince Fleming <vince@weka.io>
2022-01-13 12:29:10 -0500

    initial commit

--- build_pybin.sh
+++ build_pybin.sh
@@ -1,11 +1,11 @@
 # this file uses pyinstaller to create the binary tarball that is used to deploy the tool binary
 # this allows the tool binary to be deployed without installing python and other required python packages
 #
-TOOL=config
-MAIN=main.py
+TOOL=`basename $PWD`
+MAIN=$TOOL.py
 TARGET=tarball/$TOOL
 
-pyinstaller --onefile $MAIN
+pyinstaller --hidden-import npyscreen --onefile $MAIN
 
 mkdir -p $TARGET
 cp dist/$TOOL $TARGET
