a4f03b89d6522cb1df2c39f6a05c5aae1724b9b3
Vince Fleming <vince@weka.io>
2022-05-08 08:41:17 -0400

    Add our own TERMINFO for Ubuntu

--- wekaconfig.py
+++ wekaconfig.py
@@ -30,7 +30,8 @@ if __name__ == '__main__':
 
     # hack for Ubuntu's broken definition of xterm-256color
     if os.environ["TERM"] == "xterm-256color" and not os.path.exists("/usr/share/terminfo/x/xterm-256color"):
-        os.environ["TERMINFO"] = os.getcwd() + 'terminfo'   # we carry our own definition
+        os.environ["TERMINFO"] = f"{os.getcwd()}/terminfo"   # we carry our own definition
+        print(f"Setting TERMINFO to {os.environ['TERMINFO']}")
 
     if args.host == "localhost":
         import platform
