fda3233db41562f911655717a7416a6ee059eb45
Vince Fleming <vince@weka.io>
2022-05-29 09:21:40 -0400

    changed ssh module

--- build_pybin.sh
+++ build_pybin.sh
@@ -6,28 +6,6 @@ MAIN=$TOOL.py
 TARGET=tarball/$TOOL
 
 pyinstaller --hidden-import npyscreen \
-            --hidden-import ssh2.agent \
-            --hidden-import ssh2.pkey \
-            --hidden-import ssh2.exceptions \
-            --hidden-import ssh2.sftp \
-            --hidden-import ssh2.sftp_handle \
-            --hidden-import ssh2.channel \
-            --hidden-import ssh2.listener \
-            --hidden-import ssh2.statinfo \
-            --hidden-import ssh2.knownhost \
-            --hidden-import ssh2.error_codes \
-            --hidden-import ssh2.fileinfo \
-            --hidden-import ssh2.utils \
-            --hidden-import ssh2.publickey \
-            --hidden-import ssh.channel \
-            --hidden-import ssh.utils \
-            --hidden-import ssh.connector \
-            --hidden-import ssh.keytypes \
-            --hidden-import ssh.sftp \
-            --hidden-import ssh.sftp_attributes \
-            --hidden-import ssh.sftp_handles \
-            --hidden-import ssh.sftp_statvfs \
-            --hidden-import ssh.scp \
             --add-data terminfo:terminfo \
             --onefile $MAIN
 
