3ef3cb00a9db3e33f757324eb03bc6910321be06
Vince Fleming <46109871+vince-weka@users.noreply.github.com>
2022-12-19 19:03:23 -0500

    MCB logic, output

--- forms.py
+++ forms.py
@@ -184,7 +184,7 @@ class SelectCoresForm(PrevDoneForm):
         PA.dedicated = True if 0 in self.misc_field.value else False
         PA.auto_failure_domain = True if 1 in self.misc_field.value else False
         PA.cloud_enable = True if 2 in self.misc_field.value else False
-        if self.multicontainer:
+        if PA.Multicontainer:
             PA.num_containers_per_host = 3
             if PA.selected_cores.drives > 19:
                 PA.num_containers_per_host += 1
