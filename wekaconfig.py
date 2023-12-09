11153984b5b93f6f6ea22a580cf801f0c3f7551a
Vince Fleming <46109871+vince-weka@users.noreply.github.com>
2023-12-09 16:09:15 -0500

    VT-215

--- wekaconfig.py
+++ wekaconfig.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.version:
-        print(f"{progname} version 2.3.5")
+        print(f"{progname} version 2.3.6")
         sys.exit(0)
 
     if args.verbosity == 1:
